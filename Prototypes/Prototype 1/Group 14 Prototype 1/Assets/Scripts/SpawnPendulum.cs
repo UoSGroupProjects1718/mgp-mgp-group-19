338ec0a9d900160315c3641b63f593f4b26d177f
unknown <petrut1997@yahoo.com>
2018-03-06 23:54:56 +0200

    Working version without assets

---
+++ Prototypes/Prototype 1/Group 14 Prototype 1/Assets/Scripts/SpawnPendulum.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPendulum : MonoBehaviour {
+    public GameObject pendulum;
+
+    public void Spawn()
+    {
+        Vector3 Position = new Vector3(0, 0.89f, -8.66f);
+        
+        pendulum =Instantiate(Resources.Load("Pendulum"), Position, Quaternion.identity) as GameObject;
+        pendulum.name = "Pendulum1";
+    }
+  
+ }
+
